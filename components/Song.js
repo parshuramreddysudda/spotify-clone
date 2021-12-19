7e4117de497eac58399e7cea58bfab99705a229d
Parshuarm Reddy <parshuram.sudda@gmail.com>
2021-12-19 21:20:42 +0530

    few updates

--- components/Song.js
+++ components/Song.js
@@ -1,15 +1,32 @@
 
+import { useRecoilState, useRecoilValue } from 'recoil'
 import millisToMinutesAndSeconds from '../lib/time'
 
+import { isPlayingState, currentTrackIdState } from '../atoms/songAtom'
+import useSpotify from '../hooks/useSpotify';
+
 function Song({ order, track }) {
+
+    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+    const spotifyAPI=useSpotify();
+
+    const playSong = () => {
+        setCurrentTrackId(track.track.id);
+        setIsPlaying(true);
+        console.log(track)
+        spotifyAPI.play({
+            urls:[track.track.uri],
+        })
+    }
     return (
-        <div className="grid grid-cols-2 px-5 py-3 text-gray-500 rounded-lg cursor-pointer hover:bg-gray-900">
+        <div className="grid grid-cols-2 px-5 py-3 text-gray-500 rounded-lg cursor-pointer hover:bg-gray-900" onClick={playSong}>
             <div className="flex items-center space-x-4">
                 <p>{order + 1}</p>
                 <img className="w-10 h-10 " src={track.track.album.images[0]?.url} alt="" />
 
                 <div>
-                    <p  className='text-white truncate w-36 lg:w-64'>{track.track.name}</p>
+                    <p className='text-white truncate w-36 lg:w-64'>{track.track.name}</p>
                     <p className="w-48 truncate">{track.track.artists[0].name}</p>
                 </div>
             </div>
