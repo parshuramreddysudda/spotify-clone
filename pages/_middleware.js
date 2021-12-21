42a8067cf7495cc059661d4aa16d4c0b2425b65c
Parshuarm Reddy <parshuram.sudda@gmail.com>
2021-12-21 11:55:43 +0530

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,24 +4,23 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
 
-    // token exist if the user is logged in 
+    // // token exist if the user is logged in 
 
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+    // const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    // Allow the requests if the following is true....
-    const { pathname } = req.nextUrl;
+    // // Allow the requests if the following is true....
+    // const { pathname } = req.nextUrl;
 
-    //    1) Its a request for next auth session & provider fetching
-    //    2) the token exist 
+    // //    1) Its a request for next auth session & provider fetching
+    // //    2) the token exist 
 
-    console.log("Token is", token)
 
-    if (pathname.includes('/api/auth') || token) {
-        return NextResponse.next();
-    }
-    // Redirect them to login page if nothing exist
-    if (!token && pathname !== '/login') {
-        return NextResponse.redirect('/login');
-    }
+    // if (pathname.includes('/api/auth') || token) {
+    //     return NextResponse.next();
+    // }
+    // // Redirect them to login page if nothing exist
+    // if (!token && pathname !== '/login') {
+    //     return NextResponse.redirect('/login');
+    // }
 
 }
